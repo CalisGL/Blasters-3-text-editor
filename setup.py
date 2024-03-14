aa607c83b98bd37f0f10a63a966ad6042b927925
CalisGL <115785797+CalisGL@users.noreply.github.com>
2024-03-14 15:19:47 +0100

    commit

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 import sys
 from cx_Freeze import setup, Executable
 
-build_exe_options = {"packages": ["tkinter", "json", "ttkthemes"], "include_files": ["YokaiData.json", "icon.ico"]}
+build_exe_options = {"packages": ["tkinter", "json", "ttkthemes"], "include_files": ["dialogues.json", "icon.ico"]}
 
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
 executables = [
-    Executable("GUI.py", base=base, target_name="Blasters3dbEditor.exe")
+    Executable("GUI.py", base=base, target_name="Blasters3txtEditor.exe")
 ]
 
 
 setup(
-    name="YKWB3 Editeur de BDD",
+    name="YKWB3 Editeur de dialogues",
     version="1.0",
-    description="L'éditeur de base de données pour Yo-kai Watch Blasters 3 : un fangame tah les ouf créé par Liska !",
+    description="L'éditeur de dialogeus pour Yo-kai Watch Blasters 3 : un fangame tah les ouf créé par Liska !",
     options={"build_exe": build_exe_options},
     executables=executables
 )
